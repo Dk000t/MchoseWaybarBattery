4f16cc86836a2546ad6d49fcaab1603a0cf1677f
Dk000t <92294163+Dk000t@users.noreply.github.com>
2026-04-17 19:57:57 +0200

    Refactor Battery.py
    The script could run into some issues if the Sway configuration was reloaded, leaving chromedriver and chromium zombie processes in memory.

--- Battery.py
+++ Battery.py
@@ -1,25 +1,47 @@
 import os
 import time
 import requests
+import subprocess
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import SessionNotCreatedException
 
 # Wait for a connection to be present so as not to fail to upload the battery status to waybar
 def wait_connection():
     while True:
         try:
-            response = requests.get("https://www.google.com", timeout=3)    
-            
+            response = requests.get("https://www.google.com", timeout=3)        
             if response.status_code == 200:
-                return
+                return    
                 
         except requests.RequestException:
             pass
             
         time.sleep(2)
+
+# Kill chromedriver and chromium processes
+def kill_zombie_processes():
+    subprocess.run(
+        ["killall", "chromedriver", "chromium"],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL    
+    )
+
+#Selenium can only start one session per user dir, when you reload the Sway configuration with ctrl+shift+c waybar restarts the script leaving chromedriver and chromium as zombie processes without being able to return the battery status, the same thing would happen if at the temporal activation of the script defined by the module settings in the waybar configuration and the Sway configuration is reloaded, so, if the driver has already been created, kill the processes and create the driver again.
+def create_driver(retries = 1):
+    for attempt in range(retries + 1):
+        try:
+            driver = webdriver.Chrome(options=options)
+            return driver
+        
+        except SessionNotCreatedException:
+            if attempt >= retries:
+                raise
+            
+        kill_zombie_processes()
 
 # URL of the target page to automate
 URL = "https://www.mchose.com.cn/#/detail?deviceName=MCHOSE+A7+V2+Ultra"
@@ -27,41 +49,40 @@ URL = "https://www.mchose.com.cn/#/detail?deviceName=MCHOSE+A7+V2+Ultra"
 # Get the path to the current user's home directory
 HOME = os.path.expanduser("~")
 
-# Path to the Chromium/Chrome user data directory
-CHROME_PROFILE_PATH = f"{HOME}/.config/chromium_battery"
+# Path to the chromium user data directory
+CHROMIUM_PROFILE_PATH = f"{HOME}/.config/chromium_battery"
 
-# Initialize Chrome options object
+# Initialize chromium options object
 options = Options()
 
-# Set the path to the Chrome user data directory, so Chrome uses this profile's data (cookies, bookmarks, extensions, etc.)
-options.add_argument(f"user-data-dir={CHROME_PROFILE_PATH}")
+# Set the path to the chromium user data directory, so chromium uses this profile's data (cookies, bookmarks, extensions, etc.)
+options.add_argument(f"user-data-dir={CHROMIUM_PROFILE_PATH}")
 
-# Set up Chrome options
+# Set up chromium options
 options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-gpu")
 
-try:
-    wait_connection()
-    
-    # Creates a chromium instance with the defined options
-    driver = webdriver.Chrome(options=options)
-    
-    # Set the maximum timeout, e.g. 30 seconds
-    wait = WebDriverWait(driver, 30)
-    
-    # Open the defined URL
-    driver.get(URL)
-    
-    # Wait until the element with class "name" is visible and then grab it
-    name = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".name")))
+# Create chromium driver
+driver = create_driver()
 
-    # Wait until the element with class "other-equipment" is visible and then grab it
-    battery = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".other-equipment")))
-    
-    # Print the battery icon, name and percentage
-    print("" + "  " + battery.text + " " + "-" + " " + "MCHOSE" + " " + name.text)
+# Wait for a connection to be available
+wait_connection()
 
-finally:
-    # Close chromium
-    driver.quit()
+# Open the URL
+driver.get(URL)
+
+# Avoid placeholder values ​​in the battery, waiting until an item is visible is not enough.
+time.sleep(5)
+    
+# Grab mouse name
+name = driver.find_element(By.CSS_SELECTOR, ".name")
+    
+# Grab battery status
+battery = driver.find_element(By.CSS_SELECTOR, ".other-equipment")
+    
+# Print the battery icon, name and percentage
+print("" + "  " + battery.text + " " + "-" + " " + "MCHOSE" + " " + name.text)
+
+# Close chromium
+driver.quit()
